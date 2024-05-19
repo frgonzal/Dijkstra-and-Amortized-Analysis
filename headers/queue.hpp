c50ab2876850b1e22d0aebc775b363418b12f249
Franco Gonzalez <fr.gonzalezleiva@gmail.com>
2024-05-19 15:17:40 -0400

    feat(headers/queue): header for a queue

--- headers/queue.hpp
+++ headers/queue.hpp
@@ -9,14 +9,29 @@
 • Tener acceso directo desde cada nodo (por medio de un puntero) al par que lo representa,
 además de poder modificar su distancia con la función decreaseKey (en el contexto del
 algoritmo, no existe la necesidad de aumentar la distancia, solo de reducirla).*/
-namespace queue {
-    
-    class Queue {
-    public:
-        Queue(int size);
-        std::tuple<int, double> extractMin();
-        void decreaseKey();
-    };
-}
+class Queue {
+public:
+
+    /** Constructor por defecto de la clase Queue. */
+    Queue() = default;
+
+    /** Construye una cola a partir de una cantidad de elementos. */
+    void heapify(int i);
+
+    /** Inserta un par en la cola. */
+    void push(int node, double value);
+
+    /** Extrae el par con menor distancia de la cola. */
+    std::tuple<int, double> extractMin();
+
+    /** Disminuye el valor de un nodo en la cola. 
+    *   @param node  Nodo a disminuir.
+    *   @param value Nuevo valor del nodo.
+    */
+    void decreaseKey(int node, double value);
+
+    /** Verifica si la cola está vacía. */
+    bool empty();
+};
 
 #endif
