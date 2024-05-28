aebb3abc1a513dcf66049c1d3dd357a0d708446e
Franco Gonzalez <fr.gonzalezleiva@gmail.com>
2024-05-27 20:50:54 -0400

    feat(test): new test for the results

--- src/test.cpp
+++ src/test.cpp
@@ -5,17 +5,17 @@
 #include <stdlib.h>
 #include <iostream>
 #include <float.h>
+#include <tuple>
 #include <vector>
 #include <queue>
 #include <chrono>
 #include <cstdlib>
 #include <ostream>
 #include <unistd.h>
-#include <vector>
-#include <queue>
 #include <iostream>
 #include <ctime>
 #include <chrono>
+
 
 
 
@@ -28,147 +28,132 @@ void test_graph(const Graph &g){
         q.pop();
         visited[u] = true;
         for(auto [v, w] : g.edges[u]){
-            if(!visited[v]){
+            if(!visited[v])
                 q.push(v);
-            }
         }
     }
 
     for(int u = 0; u < visited.size(); u++){
-        if(!visited[u]){
-            std::cout << "Error en el grafo" << std::endl;
-            exit(1);
-        }
+        if(!visited[u])
+            throw std::runtime_error("No conexo");
 
         for(auto [v, w] : g.edges[u]){
-            if(v < 0 || v >= g.edges.size()){
-                std::cout << "Error en el grafo" << std::endl;
-                exit(1);
-            }
+            if(v < 0 || v >= g.edges.size())
+                throw std::runtime_error("Nodo fuera de rango");
 
-            if(w <= 0 || w > 1){
-                std::cout << "Error en el grafo" << std::endl;
-                exit(1);
-            }
+            if(w <= 0 || w > 1)
+                throw std::runtime_error("Pesos fuera de rango");
 
-            if(g.getWeight(u, v) != g.getWeight(v, u)){
-                std::cout << "Error en el grafo" << std::endl;
-                exit(1);
-            }
+            if(g.getWeight(u, v) != g.getWeight(v, u))
+                throw std::runtime_error("Pesos no simetricos");
 
-            if(g.isConnectedTo(u, v) != g.isConnectedTo(v, u)){
-                std::cout << "Error en el grafo" << std::endl;
-                exit(1);
-            }
+            if(g.isConnectedTo(u, v) != g.isConnectedTo(v, u))
+                throw std::runtime_error("Aristas no simetricas");
         }
-
 
         for(int i=0; i<g.edges[u].size(); i++){
             for(int j=0; j<g.edges[u].size(); j++){
                 auto [z1, w1] = g.edges[u][i];
                 auto [z2, w2] = g.edges[u][j];
-                if(z1 == z2 && i != j){
-                    std::cout << "Error en el grafo" << std::endl;
-                    exit(1);
-                }
+                if(z1 == z2 && i != j)
+                    throw std::runtime_error("Arista repetida");
             }
         }
     }
-
-    return;
 }
 
 
+
 void test_path(const Graph &g, const std::vector<int>& paths, const std::vector<double> &distances){
-
-
     for(int i = 0; i < paths.size(); i++){
         double dist = 0.0;
         int to = i;
         int from = paths[to];
 
-        //std::cout << to << " <- ";
         while(from > -1){
 
-            if(g.isConnectedTo(from, to)){
+            if(g.isConnectedTo(from, to))
                 dist += g.getWeight(from, to);
-                std::cout << from << " - " << to << " (" << g.getWeight(from, to) << ") \n";
-            } else {
-                std::cout << "Error en la arista de " << from << " a " << to << std::endl;
-                exit(1);
-            }
-
-            //std::cout << from;
+            else 
+                throw std::runtime_error("Error en la arista");
 
             to = from;
             from = paths[to];
-            //if(from > -1)
-            //    std::cout << " <- ";
         }
-        std::cout << std::endl;
 
         if(dist != distances[i]){
             std::cout << "Error en la distancia de " << i << " a la raiz" << std::endl;
             std::cout << "Distancia esperada: " << distances[i] << std::endl;
             std::cout << "Distancia obtenida: " << dist << std::endl;
             std::cout << g.getWeight(61, 83) << std::endl;
-
-            exit(1);
+            throw std::runtime_error("Error en la distancia");
         }
     }
 }
 
 
-int test_bin_heap(Graph &g){
+template <typename T>
+void test_vectores(const std::vector<T>& v1, const std::vector<T>& v2){
+    if(v1.size() != v2.size())
+        throw std::runtime_error("Tamaño de los vectores diferentes");
+
+    for(int i = 0; i < v1.size(); i++){
+        if(v1[i] != v2[i])
+            throw std::runtime_error("Elementos diferentes");
+    }
+}
+
+
+std::tuple<std::vector<int>, std::vector<double>> test_bin_heap(Graph &g){
     queue::Heap q = queue::Heap();
 
     auto start = std::chrono::high_resolution_clock::now();
     auto [paths, distances] = dijkstra(g, &q);
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed = end - start;
-    std::cout << "time: " << elapsed.count() << "\n";
+    std::cout << "Bin Heap time: " << elapsed.count() << "\n";
 
-    test_path(g, paths, distances);
-    return elapsed.count();
+    return std::make_tuple(paths, distances);
 }
 
-int test_fib_heap(Graph &g){
+std::tuple<std::vector<int>, std::vector<double>> test_fib_heap(Graph &g){
     queue::Fibonacci q =queue::Fibonacci();
 
     auto start = std::chrono::high_resolution_clock::now();
     auto [paths, distances] = dijkstra(g, &q);
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed = end - start;
-    std::cout << "time: " << elapsed.count() << "\n";
+    std::cout << "Fib Heap time: " << elapsed.count() << "\n";
 
-    //test_path(g, paths, distances);
-    return elapsed.count();
+    return std::make_tuple(paths, distances);
+}
+
+
+void test(int i, int j, int v, int e, int seed){
+    std::cout << "====  TEST : " << i << " " << j << "  ====\n";
+
+    auto start = std::chrono::high_resolution_clock::now();
+    Graph g(v, e, seed);
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+    std::cout << "Graph creation time: " << elapsed.count() << "\n";
+
+    //test_graph(g);
+    auto [p1, d1] = test_bin_heap(g);
+    auto [p2, d2] = test_fib_heap(g);
+    test_vectores<int>(p1, p2);
+    test_vectores<double>(d1, d2);
 }
 
 int main(){
-
     for(int i=10; i<=14; i+=2){
         for(int j=16; j<=22; j++){
-            int v = (1 << i);
-            int e = (1 << j);
-
-            if(e > v*(v-1)/2) continue;
-            std::cout << "\nTesting with 2^" << i << " vertices and 2^" << j << " edges" << std::endl;
-
             for(int k=0; k<50; k++){
-                int seed = 123*i + 456*j + 789*k + 1;
-
-                std::cout << "Graph " << k << std::endl;
-                Graph g(v, e, seed);
-                test_graph(g);
-                //test_bin_heap(g);
-                test_fib_heap(g);
+                int v = 1<<i, e = 1<<j, seed = i*12345 + j*6789;
+                if(v > 1 && e >= v - 1 && e <= v*(v-1)/2)
+                    test(i, j, v, e, seed);
             }
-
         }
     }
-
-
-
     return 0;
 }
